7bebe396d7ba067bd4869c36264fab18e8d131ae
vic.kuang <vic.kuang@qiyejia360.com>
2020-10-11 21:03:47 +0800

    Week04

---
+++ Week_04/[860]柠檬水找零.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int f = 0, t = 0;
+        for (int i = 0; i < bills.length; ++i) {
+            if (bills[i] == 5) {
+                f++;
+            } else if (bills[i] == 10) {
+                if (--f < 0) return false;
+                t++;
+            } else {
+                if (f > 0 && t > 0) {
+                    f--;
+                    t--;
+                } else if (f >= 3) {
+                    f = f - 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
